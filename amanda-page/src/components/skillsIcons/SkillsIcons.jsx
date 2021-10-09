8d30bb8e600a6d269bcc09a4a8272e1ff58111dd
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-10-09 19:14:58 +0200

    added informations to skills

--- amanda-page/src/components/skillsIcons/SkillsIcons.jsx
+++ amanda-page/src/components/skillsIcons/SkillsIcons.jsx
@@ -7,48 +7,65 @@ import { Link } from 'react-router-dom'
 export default function SkillsIcons() {
   return (
     <div className="container">
-      <IconCard description="html5">
+      <IconCard title="HTML" descriptionList={['Forms', 'Semantics', 'SEO']}>
         <FontAwesomeIcon icon={['fab', 'html5']} className="__icon" />
       </IconCard>
-      <IconCard description="css3">
+      <IconCard title="Css" descriptionList={['BEM', 'Flexbox', 'Grid', 'Responsive', 'basic animations']}>
         <FontAwesomeIcon icon={['fab', 'css3']} className="__icon" />
       </IconCard>
-      <IconCard description="javascript">
+      <IconCard title="Javascript" descriptionList={['DOM manipulation', 'fetching', 'vanilla Javascript']}>
         <FontAwesomeIcon icon={['fab', 'js']} className="__icon" />
       </IconCard>
-      <IconCard description="react">
+      <IconCard
+        title="React"
+        descriptionList={['Functional components', 'hooks', 'context', 'React router', 'Basic redux', 'Basic Webpack']}
+      >
         <FontAwesomeIcon icon={['fab', 'react']} className="__icon" />
       </IconCard>
-      <IconCard description="PHP">
+      <IconCard title="PHP" descriptionList={['Classes', 'DBO', 'Basic Features']}>
         <FontAwesomeIcon icon={['fab', 'php']} className="__icon" />
       </IconCard>
 
-      <IconCard description="laravel">
+      <IconCard
+        title="Laravel"
+        descriptionList={[
+          'Sending emails',
+          'Authentication/Authorization',
+          'Eloquent ORM',
+          'API creation',
+          'Server-side rendering',
+        ]}
+      >
         <FontAwesomeIcon icon={['fab', 'laravel']} className="__icon" />
       </IconCard>
 
-      <IconCard description="sass">
+      <IconCard title="Sass" descriptionList={['Variables', 'BEM', 'Responsive Design', 'Color Manipulation', 'Mixin']}>
         <FontAwesomeIcon icon={['fab', 'sass']} className="__icon" />
       </IconCard>
 
-      <IconCard description="database">
+      <IconCard title="Database" descriptionList={['MySql', 'Relationships', 'Migrations', 'REST']}>
         <FontAwesomeIcon icon={['fas', 'database']} className="__icon" />
-        <span className="__name">database</span>
       </IconCard>
 
-      <IconCard description="nextJS">
+      <IconCard
+        title="NextJS"
+        descriptionList={['Routing', 'Functional Components', 'Basic Features', 'Introduction to Testings']}
+      >
         <img src={nextJsImage} alt=" next-js logo" className="__next-logo" />
       </IconCard>
     </div>
   )
 }
 
-function IconCard({ children, description }) {
+function IconCard({ children, descriptionList, title }) {
   return (
     <div className="__card">
       <div className="__flip">
         <div className="__face">{children}</div>
-        <div className={['__face', '--back'].join(' ')}>{description}</div>
+        <div className={['__face', '--back'].join(' ')}>
+          <h3>{title}</h3>
+          <ul>{descriptionList && descriptionList.map((description) => <li>{description}</li>)}</ul>
+        </div>
       </div>
     </div>
   )
